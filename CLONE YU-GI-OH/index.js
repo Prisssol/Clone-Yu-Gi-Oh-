7e07c4ae2e4c15a955f112cdaac0b047d26955d5
Prisssol <119016406+Prisssol@users.noreply.github.com>
2025-01-26 15:41:21 -0300

    Add files via upload

---
+++ CLONE YU-GI-OH/index.js
@@ -0,0 +1,3 @@
+// alert('oi'); para abrir um alerta e tbm testar o java
+//OBJETIVO 1 - quando clicarmos na seta de avançar temos que mostrar o proximo cartao da lista
+// passo 1 - dar um jeito de pegar o elemento HTML da seta avancar
